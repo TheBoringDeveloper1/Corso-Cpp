af70fa6e52da076df52e818c1ffa704a00fb3d08
Devvx <manuelcacciamani1@gmail.com>
2026-09-06 21:48:36 +0200

    Add files via upload

---
+++ 4| NumeroFloat/float.cpp
@@ -0,0 +1,15 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main) 
+
+int main() {
+
+    // Stampa a schermo il numero a virgola mobile
+
+    cout << 5.6;
+    return 0;
+    
+}
