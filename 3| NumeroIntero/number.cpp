db504af5eaee24ca534ac2865d75e772db9da1c5
Devvx <manuelcacciamani1@gmail.com>
2026-09-06 21:47:08 +0200

    Add files via upload

---
+++ 3| NumeroIntero/number.cpp
@@ -0,0 +1,15 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main)
+
+int main() {
+
+    // Stampa a schermo il numero
+
+    cout << 5;
+    return 0;
+    
+}
