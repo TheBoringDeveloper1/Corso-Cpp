1dbd2f88af457c2dd26bf39d48e2eee44bfc180a
Devvx <manuelcacciamani1@gmail.com>
2026-09-06 21:56:17 +0200

    Add files via upload

---
+++ 7| InputFloat/floatIn.cpp
@@ -0,0 +1,22 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main)
+
+int main() {
+
+    float num;
+
+    // Inserimento numero
+
+    cout << "Inserisci un numero in virgola mobile: ";
+    cin >> num;
+
+    // Stampa il numero
+
+    cout << "\nNumero inserito: " << num;
+    return 0;
+    
+}
