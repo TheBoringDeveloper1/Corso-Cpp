aa7220e87addd85557f0ec6f4e52cdd3f8b8b5b0
Devvx <manuelcacciamani1@gmail.com>
2026-09-06 21:49:51 +0200

    Add files via upload

---
+++ 5| Char/char.cpp
@@ -0,0 +1,15 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main) 
+
+int main() {
+
+    // Stampa a schermo il carattere
+
+    cout << 'a';
+    return 0;
+    
+}
