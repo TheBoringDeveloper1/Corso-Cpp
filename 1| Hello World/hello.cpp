9406261be9557f5c0d252b88e667c4129cd8dcbd
Devvx <manuelcacciamani1@gmail.com>
2026-09-06 21:05:25 +0200

    Add files via upload

---
+++ 1| Hello World/hello.cpp
@@ -0,0 +1,15 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main)
+
+int main() {
+
+    // Stampa a schermo la scritta
+
+    cout << "Hello World!";
+    return 0;
+    
+}
