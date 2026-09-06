38b689aed20d072dea5dce202440103500627e7f
Devvx <manuelcacciamani1@gmail.com>
2026-09-06 21:08:00 +0200

    Add files via upload

---
+++ 2| Nome/name.cpp
@@ -0,0 +1,22 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main) 
+
+int main() {
+
+    string nome;
+
+    // Inserimento nome
+
+    cout << "Come ti chiami? ";
+    cin >> nome;
+
+    // Stampa nome
+
+    cout << "\nCiao " << nome;
+    return 0;
+    
+}
