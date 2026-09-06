abc2495f9532349f27b10ac542051ecdcdc5a349
Devvx <manuelcacciamani1@gmail.com>
2026-09-06 21:51:37 +0200

    Add files via upload

---
+++ 6| InputIntero/numIn.cpp
@@ -0,0 +1,22 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main) 
+
+int main() {
+
+    int num;
+
+    // Inserimento numero
+
+    cout << "Inserisci un numero intero: ";
+    cin >> num;
+
+    // Stampa il numero
+
+    cout << "\nNumero inserito: " << num;
+    return 0;
+    
+}
