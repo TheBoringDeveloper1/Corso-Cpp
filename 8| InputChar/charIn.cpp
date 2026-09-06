3a5525ced1bcf0ca948fc468b18482e26f1be345
Devvx <manuelcacciamani1@gmail.com>
2026-09-06 21:57:42 +0200

    Add files via upload

---
+++ 8| InputChar/charIn.cpp
@@ -0,0 +1,22 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main)
+ 
+int main() {
+
+    char ch;
+
+    // Inserimento carattere
+
+    cout << "Inserisci un carattere: ";
+    cin >> ch;
+
+    // Stampa il carattere
+    
+    cout << "\nCarattere inserito: " << ch;
+    return 0;
+
+}
