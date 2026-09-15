a99b8574d146d832c67e606294a52fa16cab1d03
Devvx <manuelcacciamani1@gmail.com>
2026-09-15 17:20:42 +0200

    Add files via upload

---
+++ 17| ForHello/forHello.cpp
@@ -0,0 +1,21 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main)
+
+int main() {
+
+    // Ciclo for stampa
+
+    for (int count = 0; count < 5; count++) {
+
+        // Stampa a schermo
+        
+        cout << "\nHello!";
+
+    }
+
+    return 0;
+}
