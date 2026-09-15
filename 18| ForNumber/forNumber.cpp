526addc763d1726870321b4bd9c123a4d962e5f7
Devvx <manuelcacciamani1@gmail.com>
2026-09-15 17:22:28 +0200

    Add files via upload

---
+++ 18| ForNumber/forNumber.cpp
@@ -0,0 +1,21 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main)
+
+int main() {
+
+    // Ciclo for stampa
+
+    for (int count = 0; count < 6; count++) {
+
+        // Stampa numeri a schermo
+
+        cout << count << endl;
+
+    }
+
+    return 0;
+}
