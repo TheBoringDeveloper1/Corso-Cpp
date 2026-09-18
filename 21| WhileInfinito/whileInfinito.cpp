e5af234d9c6755a3d3226db4cd1fdff611d17c36
Devvx <manuelcacciamani1@gmail.com>
2026-09-18 18:30:57 +0200

    Add files via upload

---
+++ 21| WhileInfinito/whileInfinito.cpp
@@ -0,0 +1,20 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main)
+
+int main() {
+
+    // Ciclo infinito
+
+    while(1) {
+        
+        // Stampa infinita
+        
+        cout << "\nRipetizione infinita";
+
+    }
+
+}
