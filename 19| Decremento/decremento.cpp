9b996930638fcf14b3b521734ac7ba3a80f22a82
Devvx <manuelcacciamani1@gmail.com>
2026-09-18 18:26:03 +0200

    Add files via upload

---
+++ 19| Decremento/decremento.cpp
@@ -0,0 +1,26 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main) 
+
+int main() {
+
+    // Variabile da decrementare
+
+    int count = 10;
+
+    // Ciclo for decremento
+
+    for (count; count >= 0; count--) {
+
+        // Stampa decremento
+
+        cout << count << endl;
+
+    }
+
+    return 0;
+
+}
